4be78e0e6e59274407267b28cdbd4f9f94bc7017
Ahmed E <2152369@bdeb.qc.ca>
2023-10-12 14:13:59 -0400

    Ajout de la musique Page d'accueil 2.0

--- Javascript/JS_accueil.js
+++ Javascript/JS_accueil.js
@@ -1,5 +1,5 @@
-window.onload = function() {
-let son = new Audio('../audio/gumball_intro.mp3'); 
+function jouerAudio() {
+    let son = new Audio('audio/gumball_intro.mp3');
     son.play();
-    loop="loop"
+
 }
