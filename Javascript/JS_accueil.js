a69532f629f23303e31a99d322073e31099971ba
Pierre Higher <Highera12@gmail.com>
2023-10-22 19:26:32 -0400

    Ajout d'un bouton 'Cliquer pour commencer' sur accueil
    Ajout d'un bouton Cliquer pour commencer animé avec des couleurs qui changent. Aussi ajouté une animation de 'Bounce'.

--- Javascript/JS_accueil.js
+++ Javascript/JS_accueil.js
@@ -1,10 +1,19 @@
-let Jouer=false;
+let Jouer = false;
+
 function jouerAudio() {
     let son = new Audio('audio/gumball_intro.mp3');
-    if (Jouer==false){
-        son.loop=true;
-        son.play();    
-        Jouer=true;
+    if (Jouer == false) {
+        son.loop = true;
+        son.play();
+        Jouer = true;
     }
+}
 
+function showButtons() {
+    jouerAudio(); // Call jouerAudio() inside this function
+    document.getElementById('startText').style.display = 'none';
+    var buttons = document.getElementsByClassName('hidden');
+    for (var i = 0; i < buttons.length; i++) {
+        buttons[i].style.display = 'block';
+    }
 }
