f0fcee3b3ced34eafba7087732ca61fb20b0fb21
Ahmed E <118456267+Ledemrocma@users.noreply.github.com>
2023-10-22 21:01:19 -0400

    Finalisation de la page Accueil et regler bug configurateur

--- Javascript/JS_accueil.js
+++ Javascript/JS_accueil.js
@@ -9,11 +9,22 @@ function jouerAudio() {
     }
 }
 
+function NewGame() {
+    localStorage.removeItem("Pseudo");
+    localStorage.removeItem("Description");
+    localStorage.removeItem("Courriel");
+    localStorage.removeItem("Personnage");
+    localStorage.removeItem("Objet");
+}
+
 function showButtons() {
     jouerAudio(); // Call jouerAudio() inside this function
     document.getElementById('startText').style.display = 'none';
     var buttons = document.getElementsByClassName('hidden');
     for (var i = 0; i < buttons.length; i++) {
-        buttons[i].style.display = 'block';
-    }
+        buttons[0].style.display = 'block';
+
+    if (localStorage.getItem("Pseudo")!=null&&localStorage.getItem("Description")!=null&&localStorage.getItem("Courriel")&&null||localStorage.getItem("Personnage")&&null||localStorage.getItem("Objet")!=null)
+        buttons[1].style.display = 'block';
+}
 }
